ed8e926bf53d00a4403c37acd6b27b4e43725510
Julien Faro <jnfaro99@gmail.com>
2024-03-31 15:30:44 -0700

    Set model scale

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,12 @@ import * as THREE from "three"
 import { DRACOLoader, OrbitControls } from "three/examples/jsm/Addons.js";
 import { GLTFLoader } from "three/examples/jsm/Addons.js";
 
+const MODEL_SCALE = 0.1;
+
+const isMeshType = (object?: THREE.Object3D): object is THREE.Mesh => {
+    return object?.type === "Mesh"
+}
+
 export default class App {
     // DOM
     container: HTMLElement;
@@ -14,10 +20,12 @@ export default class App {
     camera: THREE.PerspectiveCamera;
     controls: OrbitControls;
     scene: THREE.Scene;
+    time: number;
 
     // Model
     dracoLoader: DRACOLoader;
-    gltf: GLTFLoader;
+    gltfLoader: GLTFLoader;
+    model?: THREE.Object3D<THREE.Object3DEventMap>;
 
     constructor() {
         // Create renderer.
@@ -42,12 +50,13 @@ export default class App {
         this.camera = new THREE.PerspectiveCamera(70, this.width / this.height, 0.001, 1000);
         this.camera.position.set(0, 0, 2);
         this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+        this.time = 0;
 
         // Setup model.
         this.dracoLoader = new DRACOLoader();
         this.dracoLoader.setDecoderPath("/draco/");
-        this.gltf = new GLTFLoader();
-        this.gltf.setDRACOLoader(this.dracoLoader);
+        this.gltfLoader = new GLTFLoader();
+        this.gltfLoader.setDRACOLoader(this.dracoLoader);
 
         // Handle window resize.
         this.handleResize();
@@ -67,17 +76,34 @@ export default class App {
     }
 
     animate() {
-        this.renderer.render(this.scene, this.camera)
+        this.time += 0.05;
         requestAnimationFrame(() => this.animate());
+        this.renderer.render(this.scene, this.camera)
+    }
+
+    addLights() {
+        const ambient = new THREE.AmbientLight(0xffffff, 0.5);
+        this.scene.add(ambient);
+
+        const directional = new THREE.DirectionalLight(0xffffff, 0.5);
+        directional.position.set(0.5, 0, 0.866);
+        this.scene.add(directional)
     }
 
     addObjects() {
-        this.gltf.load(
+        this.gltfLoader.load(
             // Resource URL
             "human.glb",
             (gltf) => {
                 console.log("loaded", gltf)
                 this.scene.add(gltf.scene);
+                this.model = gltf.scene.children[0];
+                this.model.scale.set(MODEL_SCALE, MODEL_SCALE, MODEL_SCALE);
+                this.model.rotation.set(0, 0, 0);
+
+                if (isMeshType(this.model) && this.model.isMesh) {
+                    this.model.geometry.center();
+                }
             },
             (xhr) => {
                 console.log((xhr.loaded / xhr.total * 100) + '% loaded')
