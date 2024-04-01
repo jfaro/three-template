35c1ddc95ff7ae647d8b12c6786c9427000cdd4d
Julien Faro <jnfaro99@gmail.com>
2024-03-31 17:02:29 -0700

    Bloom postprocessing

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
 import * as THREE from "three"
-import { DRACOLoader, OrbitControls } from "three/examples/jsm/Addons.js";
-import { GLTFLoader } from "three/examples/jsm/Addons.js";
+import {
+    DRACOLoader,
+    GLTFLoader,
+    OrbitControls,
+    RenderPass,
+    UnrealBloomPass,
+    EffectComposer,
+    OutputPass
+} from "three/examples/jsm/Addons.js";
 
 const MODEL_SCALE = 0.1;
 
@@ -16,26 +23,28 @@ export default class App {
 
     // Scene properties.
     renderer: THREE.WebGLRenderer;
-    clock: THREE.Clock;
     camera: THREE.PerspectiveCamera;
     controls: OrbitControls;
     scene: THREE.Scene;
     time: number;
 
-    // Env
+    // Environment.
     envMap?: THREE.Texture;
+    composer: EffectComposer;
 
-    // Model
+    // 3D Model.
     dracoLoader: DRACOLoader;
     gltfLoader: GLTFLoader;
-    model?: THREE.Mesh;
-    pmremGenerator?: THREE.PMREMGenerator;
+    textureLoader: THREE.TextureLoader;
+    pmremGenerator: THREE.PMREMGenerator;
+    model?: THREE.Mesh; // Null in the case resource loading fails.
 
     constructor() {
         // Create renderer.
         this.renderer = new THREE.WebGLRenderer();
         this.renderer.setPixelRatio(window.devicePixelRatio);
-        this.renderer.setClearColor(0xeeeeee, 1);
+        this.renderer.setClearColor(0x000000, 1);
+        this.renderer.toneMapping = THREE.ACESFilmicToneMapping;
 
         // Add renderer to DOM.
         const container = document.getElementById("app");
@@ -48,12 +57,27 @@ export default class App {
             throw new Error("DOM contains no 'app' element")
         }
 
-        // Create scene.
+        // Scene and camera.
         this.scene = new THREE.Scene();
-        this.clock = new THREE.Clock();
         this.camera = new THREE.PerspectiveCamera(70, this.width / this.height, 0.001, 1000);
         this.camera.position.set(0, 0, 2);
+
+        // Camera controls.
         this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+        this.controls.maxPolarAngle = Math.PI * 0.9;
+        this.controls.minPolarAngle = Math.PI * 0.1;
+        this.controls.minDistance = 1;
+        this.controls.maxDistance = 5;
+
+        // Postprocessing.
+        const renderScene = new RenderPass(this.scene, this.camera);
+        const resolution = new THREE.Vector2(this.width, this.height);
+        const bloomPass = new UnrealBloomPass(resolution, 1.5, 0.8, 0.3);
+        const outputPass = new OutputPass();
+        this.composer = new EffectComposer(this.renderer);
+        this.composer.addPass(renderScene);
+        this.composer.addPass(bloomPass);
+        this.composer.addPass(outputPass);
         this.time = 0;
 
         // Setup model.
@@ -61,12 +85,16 @@ export default class App {
         this.dracoLoader.setDecoderPath("/draco/");
         this.gltfLoader = new GLTFLoader();
         this.gltfLoader.setDRACOLoader(this.dracoLoader);
+        this.pmremGenerator = new THREE.PMREMGenerator(this.renderer);
+        this.pmremGenerator.compileEquirectangularShader();
+        this.textureLoader = new THREE.TextureLoader();
 
         // Handle window resize.
         this.handleResize();
-        window.addEventListener("resize", () => this.handleResize(), false);
+        window.addEventListener("resize", () => this.handleResize());
 
         this.addObjects();
+        // this.addLights();
 
         // Begin render loop.
         this.animate();
@@ -76,6 +104,7 @@ export default class App {
         this.width = this.container.offsetWidth;
         this.height = this.container.offsetHeight;
         this.renderer.setSize(this.width, this.height)
+        this.composer.setSize(this.width, this.height)
         this.camera.aspect = this.width / this.height
         this.camera.updateProjectionMatrix();
     }
@@ -83,24 +112,19 @@ export default class App {
     animate() {
         this.time += 0.05;
         requestAnimationFrame(() => this.animate());
-        this.renderer.render(this.scene, this.camera)
+        this.composer.render()
     }
 
     addLights() {
-        const ambient = new THREE.AmbientLight(0xffffff, 0.5);
+        const ambient = new THREE.AmbientLight(0xcccccc);
         this.scene.add(ambient);
-        const directional = new THREE.DirectionalLight(0xffffff, 0.5);
-        directional.position.set(0.5, 0, 0.866);
-        this.scene.add(directional)
+        const pointLight = new THREE.PointLight(0xffffff, 0.2);
+        this.scene.add(pointLight)
     }
 
     addObjects() {
         // Load environment map.
-        this.pmremGenerator = new THREE.PMREMGenerator(this.renderer);
-        this.pmremGenerator.compileEquirectangularShader();
-
-        const textureLoader = new THREE.TextureLoader();
-        textureLoader.load(
+        this.textureLoader.load(
             "environment-map.jpg",
             texture => {
                 console.log("Done loading env map");
@@ -134,10 +158,14 @@ export default class App {
                 this.model.rotation.set(0, 0, 0);
                 this.model.geometry.center();
 
+                // Define model material.
                 const material = new THREE.MeshStandardMaterial({
                     metalness: 1,
                     roughness: 0.28
                 });
+                // material.onBeforeCompile(shader => {
+
+                // })
                 if (this.envMap) {
                     material.envMap = this.envMap;
                 }
